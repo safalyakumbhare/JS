0e921eab95b5ab85fa286e9658f2170f7e176e98
Safalya Kumbhare <109536244+safalyakumbhare@users.noreply.github.com>
2024-06-13 14:01:34 +0530

    Accessing Elements using DOM

--- DOM/script.js
+++ DOM/script.js
@@ -1,2 +1,19 @@
 // console.dir(window)
-document.body.childNodes[3].innerText = "ram ram"
+// document.body.childNodes[3].innerText = "ram ram"  //update the text on the html using JS
+
+
+/* Accessing Elements using DOM */
+// let heading = document.getElementById("heading");  //used to access elements by ID
+// console.dir(heading)
+
+// let head2 = document.getElementsByClassName("heading2");  //used to access using Class Name
+// console.dir(head2);
+
+// let para = document.getElementsByTagName("p");  // used to access the elements by there tags
+// console.dir(para)
+
+// let p1 = document.querySelector("p");  //this DOM can access the all types of elements like id,class or tags and return the first element only
+// console.dir(p1);
+
+let p_all = document.querySelectorAll("p"); //this DOM can return the Nodelist (all the given elements) on the web page 
+console.dir(p_all);
